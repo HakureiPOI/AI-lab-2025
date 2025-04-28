b78572953706cd6102d8f8998ede94fe1457641b
HakureiPOI <hakureipoi@qq.com>
2025-04-28 12:56:27 +0800

    load_mnist

--- lab2/src/dataset/mnist.py
+++ lab2/src/dataset/mnist.py
@@ -1,71 +1,46 @@
 import os
 import pickle
 import gzip
-import numpy as np
 import urllib.request
+import numpy as np
 
-def load_mnist(normalize=True, flatten=False, one_hot_label=False):
-    url = 'http://yann.lecun.com/exdb/mnist/'
-    key_file = {
-        'train_img': 'train-images-idx3-ubyte.gz',
-        'train_label': 'train-labels-idx1-ubyte.gz',
-        'test_img': 't10k-images-idx3-ubyte.gz',
-        'test_label': 't10k-labels-idx1-ubyte.gz'
-    }
-    dataset_dir = os.path.dirname(__file__)  # 当前文件夹
-    save_file = os.path.join(dataset_dir, 'mnist.pkl')
+def load_mnist(normalize=True, flatten=True, one_hot_label=False):
+    url = "https://github.com/mnielsen/neural-networks-and-deep-learning/raw/master/data/mnist.pkl.gz"
+    save_path = "mnist.pkl.gz"
+    
+    # 如果本地没有，就下载
+    if not os.path.exists(save_path):
+        print(f"Downloading {save_path} from {url}...")
+        urllib.request.urlretrieve(url, save_path)
+        print("Download finished!")
 
-    # 如果 pkl 文件存在，直接读取
-    if os.path.exists(save_file):
-        with open(save_file, 'rb') as f:
-            dataset = pickle.load(f)
-    else:
-        # 检查是否有数据文件，没有就下载
-        for v in key_file.values():
-            file_path = os.path.join(dataset_dir, v)
-            if not os.path.exists(file_path):
-                print(f"Downloading {v}...")
-                urllib.request.urlretrieve(url + v, file_path)
+    # 读取数据
+    with gzip.open(save_path, 'rb') as f:
+        train_set, val_set, test_set = pickle.load(f, encoding='latin1')
+        
+    # 只用训练集、测试集（忽略验证集）
+    train_img, train_label = train_set
+    test_img, test_label = test_set
 
-        # 读取数据
-        dataset = {}
-        dataset['train_img'] = _load_imgs(os.path.join(dataset_dir, key_file['train_img']))
-        dataset['train_label'] = _load_labels(os.path.join(dataset_dir, key_file['train_label']))
-        dataset['test_img'] = _load_imgs(os.path.join(dataset_dir, key_file['test_img']))
-        dataset['test_label'] = _load_labels(os.path.join(dataset_dir, key_file['test_label']))
+    # 处理 normalize
+    if normalize:
+        train_img = train_img.astype(np.float32)
+        test_img = test_img.astype(np.float32)
 
-        # 保存成 pkl 文件
-        with open(save_file, 'wb') as f:
-            pickle.dump(dataset, f, -1)
+    # 处理 flatten
+    if not flatten:
+        train_img = train_img.reshape(-1, 28, 28)
+        test_img = test_img.reshape(-1, 28, 28)
 
-    # 处理 normalize / flatten / one_hot_label
-    train_img, train_label = _preprocess(dataset['train_img'], dataset['train_label'], normalize, flatten, one_hot_label)
-    test_img, test_label = _preprocess(dataset['test_img'], dataset['test_label'], normalize, flatten, one_hot_label)
+    # 处理 one_hot_label
+    if one_hot_label:
+        train_label = _to_one_hot(train_label)
+        test_label = _to_one_hot(test_label)
 
     return (train_img, train_label), (test_img, test_label)
 
-def _load_imgs(file_path):
-    with gzip.open(file_path, 'rb') as f:
-        data = np.frombuffer(f.read(), np.uint8, offset=16)
-    return data.reshape(-1, 28, 28)
-
-def _load_labels(file_path):
-    with gzip.open(file_path, 'rb') as f:
-        labels = np.frombuffer(f.read(), np.uint8, offset=8)
-    return labels
-
-def _preprocess(imgs, labels, normalize, flatten, one_hot_label):
-    if normalize:
-        imgs = imgs.astype(np.float32)
-        imgs /= 255.0
-    if flatten:
-        imgs = imgs.reshape(imgs.shape[0], -1)  # (N, 784)
-    if one_hot_label:
-        labels = _to_one_hot(labels)
-    return imgs, labels
-
 def _to_one_hot(labels, num_classes=10):
     one_hot = np.zeros((labels.size, num_classes))
-    for idx, row in enumerate(one_hot):
-        row[labels[idx]] = 1
+    for idx, label in enumerate(labels):
+        one_hot[idx, label] = 1
     return one_hot
