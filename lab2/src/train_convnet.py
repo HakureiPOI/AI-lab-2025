caace71f91292dee9fed4c1b4bc1bb9566f60e24
HakureiPOI <114349635+HakureiPOI@users.noreply.github.com>
2025-05-05 15:12:02 +0800

    Update train_convnet.py

--- lab2/src/train_convnet.py
+++ lab2/src/train_convnet.py
@@ -12,8 +12,8 @@ import time
 # 读入数据
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=False)
 
-x_train = x_train[:2000]
-x_test = x_test[:2000]
+x_train = x_train[:500]
+x_test = x_test[:500]
 
 if x_train.ndim == 3:
     x_train = x_train[:, np.newaxis, :, :]
