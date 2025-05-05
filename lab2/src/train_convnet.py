856bf29f7c344b34ec516767ec648bbceecc473b
HakureiPOI <114349635+HakureiPOI@users.noreply.github.com>
2025-05-05 14:13:05 +0800

    lr=1e-3

--- lab2/src/train_convnet.py
+++ lab2/src/train_convnet.py
@@ -19,7 +19,7 @@ if x_train.ndim == 3:
 network = SimpleConvNet()
 train_size = x_train.shape[0]
 batch_size = 100
-learning_rate = 0.02
+learning_rate = 0.001
 print('train size', train_size)
 
 train_loss_list = []
