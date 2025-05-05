b48037cf606e5c62299b8b32f77ef771a1ba9373
HakureiPOI <114349635+HakureiPOI@users.noreply.github.com>
2025-05-05 15:16:22 +0800

    Update simple_convnet.py

--- lab2/src/simple_convnet.py
+++ lab2/src/simple_convnet.py
@@ -13,7 +13,7 @@ class SimpleConvNet:
     """
     def __init__(self, input_dim=(1, 28, 28), 
                  conv_param={'filter_num':30, 'filter_size':3, 'pad':0, 'stride':1},
-                 hidden_size=100, output_size=10, weight_init_std=0.01):
+                 hidden_size=100, output_size=10, weight_init_std=0.1):
         filter_num = conv_param['filter_num']
         filter_size = conv_param['filter_size']
         filter_pad = conv_param['pad']
